4ad001d5bda9ca584a5a6dc031dc63901b071ba5
voldiriddle <felix.kniese@yahoo.com>
2025-06-24 10:30:06 +0200

    added logic using print statements

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ class App:
         self.clear_whole_db()
         self.init_sq()
         self.insert_essential_data()
+        self.options = ['Kohte', 'SKohte', 'Jurtelang', 'Jurtekurz', 'Großjurte', 'Gigajurte', 'HochkohteGroß'] #Automatation later pls
+        self.command_loop()
+        
 
 
     def init_sq(self):
@@ -17,6 +20,11 @@ class App:
         self.cursor.execute(''' CREATE TABLE IF NOT EXISTS SKohte (mat TEXT, anzahl INTEGER)''')
         self.cursor.execute(''' CREATE TABLE IF NOT EXISTS Jurtelang (mat TEXT, anzahl INTEGER)''')
         self.cursor.execute(''' CREATE TABLE IF NOT EXISTS Jurtekurz (mat TEXT, anzahl INTEGER)''')
+        self.cursor.execute(''' CREATE TABLE IF NOT EXISTS Großjurte (mat TEXT, anzahl INTEGER)''')
+        self.cursor.execute(''' CREATE TABLE IF NOT EXISTS Gigajurte (mat TEXT, anzahl INTEGER)''')
+        self.cursor.execute(''' CREATE TABLE IF NOT EXISTS HochkohteGroß (mat TEXT, anzahl INTEGER)''')
+        self.cursor.execute(''' CREATE TABLE IF NOT EXISTS Gesamt (mat TEXT, anzahl INTEGER)''')
+
         self.conn.commit()
 
     def execute_command(self, text):
@@ -53,7 +61,80 @@ class App:
         self.execute_command('INSERT INTO Jurtelang (mat, anzahl) VALUES ("Seitenstange 2M", 12)')
         self.execute_command('INSERT INTO Jurtelang (mat, anzahl) VALUES ("Heringe", 12)')
         self.execute_command('INSERT INTO Jurtelang (mat, anzahl) VALUES ("Tampen", 15)')
+        self.execute_command('INSERT INTO Jurtelang (mat, anzahl) VALUES ("Lange Seile", 3)')
 
+        self.execute_command('INSERT INTO Jurtekurz (mat, anzahl) VALUES ("12er Jurtendach", 1)')
+        self.execute_command('INSERT INTO Jurtekurz (mat, anzahl) VALUES ("6er Spinne", 1)')
+        self.execute_command('INSERT INTO Jurtekurz (mat, anzahl) VALUES ("Seitenplane 1.6M", 12)')
+        self.execute_command('INSERT INTO Jurtekurz (mat, anzahl) VALUES ("Seitenstange 1.6M", 12)')
+        self.execute_command('INSERT INTO Jurtekurz (mat, anzahl) VALUES ("Heringe", 12)')
+        self.execute_command('INSERT INTO Jurtekurz (mat, anzahl) VALUES ("Tampen", 15)')
+        self.execute_command('INSERT INTO Jurtekurz (mat, anzahl) VALUES ("Lange Seile", 3)')
+
+        self.execute_command('INSERT INTO Großjurte (mat, anzahl) VALUES ("16er Jurtendach", 1)')
+        self.execute_command('INSERT INTO Großjurte (mat, anzahl) VALUES ("8er Spinne", 1)')
+        self.execute_command('INSERT INTO Großjurte (mat, anzahl) VALUES ("Seitenplane 2M doppelt", 8)')
+        self.execute_command('INSERT INTO Großjurte (mat, anzahl) VALUES ("Seitenstange 2M", 16)')
+        self.execute_command('INSERT INTO Großjurte (mat, anzahl) VALUES ("Heringe", 16)')
+        self.execute_command('INSERT INTO Großjurte (mat, anzahl) VALUES ("Tampen", 20)')
+        self.execute_command('INSERT INTO Großjurte (mat, anzahl) VALUES ("Lange Seile", 3)')
+
+        self.execute_command('INSERT INTO Gigajurte (mat, anzahl) VALUES ("18er Jurtendach (Giga)", 1)')
+        self.execute_command('INSERT INTO Gigajurte (mat, anzahl) VALUES ("9er Spinne", 1)')
+        self.execute_command('INSERT INTO Gigajurte (mat, anzahl) VALUES ("Seitenplane 2M doppelt", 9)')
+        self.execute_command('INSERT INTO Gigajurte (mat, anzahl) VALUES ("Seitenstange 2M", 18)')
+        self.execute_command('INSERT INTO Gigajurte (mat, anzahl) VALUES ("Heringe", 18)')
+        self.execute_command('INSERT INTO Gigajurte (mat, anzahl) VALUES ("Tampen", 22)')
+        self.execute_command('INSERT INTO Gigajurte (mat, anzahl) VALUES ("Lange Seile", 3)')
+
+        self.execute_command('INSERT INTO HochkohteGroß (mat, anzahl) VALUES ("Kohtenplane", 4)')
+        self.execute_command('INSERT INTO HochkohteGroß (mat, anzahl) VALUES ("Seitenplane 1.6M", 16)')
+        self.execute_command('INSERT INTO HochkohteGroß (mat, anzahl) VALUES ("Lange Seile", 3)')
+        self.execute_command('INSERT INTO HochkohteGroß (mat, anzahl) VALUES ("Seitenstange 3M", 8)')
+        self.execute_command('INSERT INTO HochkohteGroß (mat, anzahl) VALUES ("Heringe", 8)')
+        self.execute_command('INSERT INTO HochkohteGroß (mat, anzahl) VALUES ("Tampen", 8)')
+        self.execute_command('INSERT INTO HochkohteGroß (mat, anzahl) VALUES ("Kohtenabdeckplane", 1)')
+        self.execute_command('INSERT INTO HochkohteGroß (mat, anzahl) VALUES ("Kohtenkreuzstange", 2)')
+
+    def add_to_total(self, obj):
+        if obj not in ['Kohte', 'SKohte', 'Jurtelang', 'Jurtekurz', 'Großjurte', 'Gigajurte', 'HochkohteGroß']:
+            print('Object not valid')
+            return
+
+        self.cursor.execute(f"SELECT mat, anzahl FROM {obj}")
+        rows = self.cursor.fetchall()
+
+        for mat, anzahl in rows:
+            self.cursor.execute("SELECT anzahl FROM Gesamt WHERE mat = ?", (mat,))
+            result = self.cursor.fetchone()
+
+
+
+            if result:
+                new_ges = result[0] + anzahl
+                self.cursor.execute("UPDATE Gesamt SET anzahl = ? WHERE mat = ?", (new_ges, mat))
+
+            else:
+                self.cursor.execute("INSERT INTO Gesamt (mat, anzahl) VALUES (?, ?)", (mat, anzahl))
+
+        self.conn.commit()
+        print(f"Added materials from {obj} to Gesamt.")
+
+
+
+    def command_loop(self):
+        running = True
+        while running:
+            print("What do you want to add? ")
+            for i in range(len(self.options)):
+                print(f'Enter {i+1} to add a {self.options[i]}')
+
+            x = input("Enter here : ")
+            print(self.options[int(x)-1])
+            self.add_to_total(self.options[int(x)-1])
+
+            if input("Do you want to add more? (y/n)").lower() == 'n':
+                running = False
 
 
 
